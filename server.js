0395402f2aaf83da45f9ffea5e1bd2db59146702
Otabek <otabek.akbaroff@gmail.com>
2020-03-23 15:44:40 -0400

    some endpoints are done

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const routerOne=require('./routes/routeOne')
+
+const assessments=require('./routes/assessments/assessments')
+
+const point_data=require('./routes/points_data/points_data')
+
+server.use('/one', routerOne);
+
+
+server.use('/assessments', assessments);
+
+server.use('/point_data', point_data);
+
+server.get('/', (req,res)=>{
+
+    const test=[{Message:`*** THIS GET WORKS :) ***`}]
+
+    res.status(200).json(test);
+
+})
+
+
+module.exports = server;
