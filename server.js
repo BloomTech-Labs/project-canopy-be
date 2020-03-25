0ff90b8df15c5196ec6c2e95e0bf8423e5d2e060
Otabek <otabek.akbaroff@gmail.com>
2020-03-25 19:22:17 -0400

    removed clones

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const cors=require('cors');
 
 
 const express = require('express');
-const cors = require('cors');
+
 const server = express();
 
 server.use(express.json());
-server.use(cors());
 
 server.use(cors());
 
