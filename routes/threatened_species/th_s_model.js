831891ac9e197dc9fd873c135a67865220e6818d
Otabek <otabek.akbaroff@gmail.com>
2020-04-07 17:59:57 -0400

    changes to db

--- routes/threatened_species/th_s_model.js
+++ routes/threatened_species/th_s_model.js
@@ -3,8 +3,10 @@ const db=require('../../data/dbconfig');
 
 module.exports = {
     tax,
-    UniqueClassNames,
-    classNameCount
+    classNameCount,
+    countryClassCounts,
+    classCountByCountry,
+    // classCountAllCountries
 };
 
 function tax(){
@@ -14,18 +16,132 @@ function tax(){
     .join('taxonomy as t', "a.internalTaxonId", "t.internalTaxonId");
 }
 
-function UniqueClassNames(){
-    return db('assessments as a')
-    .distinct("t.className")
-    .where("a.redlistCategory","Critically Endangered").orWhere("a.redlistCategory","Endangered").orWhere("a.redlistCategory","Vulnerable")
-    .join('taxonomy as t', "a.internalTaxonId", "t.internalTaxonId");
-}
-
-
-function classNameCount(name){
+function classNameCount(){
     return db('assessments as a')
     .select("t.className")
-    .where("a.redlistCategory","Critically Endangered").orWhere("a.redlistCategory","Endangered").orWhere("a.redlistCategory","Vulnerable")
-    .count("t.className", `${name}`)
+    .count('t.className')
+    .groupBy('t.className')
+    .where("a.redlistCategory","Critically Endangered")
+        .orWhere("a.redlistCategory","Endangered")
+        .orWhere("a.redlistCategory","Vulnerable")
     .join('taxonomy as t', "a.internalTaxonId", "t.internalTaxonId");
 }
+
+function countryClassCounts(){
+    // db('assessments as a')
+    // .select("t.className")
+    // .count('t.className')
+    // .groupBy('t.className')
+    // .where("a.redlistCategory","Critically Endangered")
+    //     .orWhere("a.redlistCategory","Endangered")
+    //     .orWhere("a.redlistCategory","Vulnerable")
+    // .join('taxonomy as t', "a.internalTaxonId", "t.internalTaxonId")
+    // .join("countries as c", "a.internalTaxonId", "c.internalTaxonId");
+}
+
+
+//============== HELPER FUNCTION ================
+
+function classCountByCountry(country){
+    return db('assessments as a')
+        .select("t.className", "c.name")
+        .count('t.className as classCount')
+        .groupBy('t.className')
+        .whereIn("a.redlistCategory",["Critically Endangered", "Endangered", "Vulnerable"])
+        .andWhere('c.name', country)
+        .join('taxonomy as t', "a.internalTaxonId", "t.internalTaxonId")
+        .join("countries as c", "a.internalTaxonId", "c.internalTaxonId")
+    .then(countryCount => {
+        const counts = countryCount.map(item => {
+            return {
+                className: item.className,
+                count: item.classCount
+            }
+        })
+        return {
+            country: country,
+            counts: counts
+        }
+    })
+    // return {"banana": "apple"}
+}
+
+// function classCountAllCountries() {
+//     const crbCountries = ['Cameroon', 'Congo, The Democratic Republic of the', 'Gabon', 'Congo', 'Central African Republic', 'Equatorial Guinea'];
+
+//     const allClassCountryCounts = [];
+
+//     crbCountries.map(country => {
+//         classCountByCountry(country)
+//             .then(countryCount => {
+//                 allClassCountryCounts.push(countryCount)
+//             })
+//     });
+//     return allClassCountryCounts;
+// }
+
+
+
+// function classCountAllCountries(){
+//     const crbCountries = ['Cameroon', 'Congo, The Democratic Republic of the', 'Gabon', 'Congo', 'Central African Republic', 'Equatorial Guinea'];
+
+//     let allClassCountryCounts = crbCountries.map(country => {
+//         return classCountByCountry(country)
+//         .then((count) => {
+//             return count
+//         })
+//     });
+
+//     // return allClassCountryCounts;
+//     // return [1, 2, 3]
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function returnClassCountsByACountry(country){
+//     db() // returns taxonomy class counts for a country
+//         .then((taxonomyCountsOfACountry) => {
+//             return {
+//                 country: country, //string
+//                 taxonomyCounts: taxonomyCounts //array
+//             }
+//         })
+// }
+
+// function returnClassCountsForCongoRiverBasis(){
+//     let crb = [
+//         "Republic of Congo",
+//         "Cameroon",
+//         "Gabon"
+//     ]
+
+//     let taxonomyCounts = []
+
+//     crb.forEach(item => {
+//         return ClassCountsByACountry(item)
+//             .then((countryClassCount)=> {
+//                 taxonomyCounts.push(countryClassCount)
+//             })
+//     })
+
+//     return taxonomyCounts
+// }
+
+
+
+
--- routes/threatened_species/th_s_route.js
+++ routes/threatened_species/th_s_route.js
@@ -12,34 +12,51 @@ router.get('/tax', (req, res) => {
     })
 });
 
-router.get('/tax/class', (req, res) => {
-    Th_s_model.UniqueClassNames().then(user=>{
-        let destictClases=user.map(item=>{
-            return item.className;
+router.get('/tax/classCount', (req, res) => {
+    Th_s_model.classNameCount()
+        .then(classCount => {
+            res.status(200).json(classCount)
         })
-        Th_s_model.tax().then(res=>{
-            res.filter(item=>{
-               
-            })
+        .catch(err => {
+            res.status(500).json({err})
         })
-        console.log(destictClases);
-        res.status(200).json(
-            user
-            );
-    }).catch(err=>{
-        console.log(err);
-        res.status(500).json({errorMessage:'Can\'t get Users Because there\'s something wrong with DATABASE'})
-    })
-});
+})
 
-router.get('/tax/class/count', (req, res) => {
-    Th_s_model.UniqueClassNames()
-        .then(UniqueClasses => {
-            UniqueClasses.map(count=>{
-                Th_s_model.classNameCount(count.className)
-            })
-        })
-        
+// router.post('/tax/classCount',(req,res)=>{
+//     let searchBasis=req.body.searchBasis;
+//     res.status(200).json({searchBasis:"whatever"})
+// });
+
+// router.get('/test', (req, res) => {
+//     // Th_s_model.classCountAllCountries()
+//     //     .then(count => {
+//     //         res.status(200).json(count)
+//     //     })
+//     // res.json(Th_s_model.classCountByCountry('Kenya'));
+//     // res.json(Th_s_model.classCountAllCountries());
+//     Th_s_model.classCountByCountry('Kenya')
+//     .then((resp) => {
+//         res.json({resx: resp});
+//     })
+  
+// })
+
+
+
+router.get('/test', (req, res) => {
+    // Th_s_model.classCountAllCountries()
+    //     .then(count => {
+    //         res.status(200).json(count)
+    //     })
+    // res.json(Th_s_model.classCountByCountry('Kenya'));
+    // res.json(Th_s_model.classCountAllCountries());
+    const crbCountries = ['Cameroon', 'Congo, The Democratic Republic of the', 'Gabon', 'Congo', 'Central African Republic', 'Equatorial Guinea'];
+    for(let i=0; i<crbCountries; i++){
+        Th_s_model.classCountByCountry(crbCountries[i])
+    .then((resp) => {
+        res.json({resx: resp});
+    })
+    }
 })
 
 
