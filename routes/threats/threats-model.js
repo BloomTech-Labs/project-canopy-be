43991316de4470333351da0e7e226325c40203db
Kris Monroe <krisdmonroe@gmail.com>
2020-04-09 15:51:46 -0700

    Clean up code and added comments

--- routes/threats/threats-model.js
+++ routes/threats/threats-model.js
@@ -2,146 +2,15 @@ const knex = require('knex');
 const knexConfig = require('../../knexfile');
 const db = knex(knexConfig.development);
 
+module.exports = {
+    findThreatenedBy
+};
+
 const habitatCodes = [1.5, 1.6, 1.7, 1.8, 1.9, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 14.6];
 
 const crbArry = ['Cameroon', 'Congo, The Democratic Republic of the', 'Gabon', 'Congo', 'Central African Republic', 'Equatorial Guinea'];
 
-module.exports = {
-    // find,
-    // findbyEndangered,
-    // findByCountry,
-    // findByhabitat,
-    // findByTaxonomicRank,
-    // promiseCheck,
-    findThreatenedBy
-};
-  
-// function find() {
-//     return db("threats");
-// }
-
-// function findbyEndangered(filter) {
-//     return db("assessments")
-//         .join("countries","assessments.scientificName", "countries.scientificName" )
-//         .join("habitats","assessments.scientificName", "habitats.scientificName")
-//         .select("assessments.scientificName as Scientific Name", "assessments.redlistCategory as Redlist Category")
-//         .whereIn("assessments.redlistCategory",["Critically Endangered", "Endangered", "Vulnerable"])
-//         .andWhere(function(){
-//             this.whereIn("countries.name", crbArry)
-//                 .andWhere(function(){
-//                     this.whereIn("habitats.code", habitatCodes)
-//                 })
-//         })
-            
-// }
-
-// function findByCountry() {
-//     return db("assessments")
-//         .join("countries","assessments.scientificName", "countries.scientificName" )
-//         .join("habitats","assessments.scientificName", "habitats.scientificName")
-//         .whereIn("assessments.redlistCategory",["Critically Endangered", "Endangered", "Vulnerable"])
-//         .andWhere(function(){
-//             this.whereIn("countries.name", crbArry)
-//                 .andWhere(function(){
-//                     this.whereIn("habitats.code", habitatCodes)
-//                 })
-//         })
-//         .distinct("countries.scientificName")
-//         .select("redlistCategory", "countries.name as country")
-//         .then(data => {
-//             const countryObj = crbArry.map(item => {
-//                 return {
-//                     country: item,
-//                     species: []
-//                 }
-//             })
-//             data.map(item => {
-//                 const species = item;
-//                 countryObj.map(obj => {
-//                     if(species.country === obj.country){
-//                         obj.species.push(species)
-//                     }
-//                 })
-//             })
-//             return countryObj
-//         })
-// }
-
-// function findByTaxonomicRank(filter){
-//     return db("assessments")
-//         .join("taxonomy", "assessments.scientificName", "taxonomy.scientificName")
-//         .join("countries", "assessments.scientificName", "countries.scientificName")
-//         .whereIn("assessments.redlistCategory",["Critically Endangered", "Endangered", "Vulnerable"])
-//         .andWhere(function(){
-//             this.whereIn("countries.name", crbArry)
-//         })
-//         .select( "countries.name as Country","taxonomy.scientificName","taxonomy.className","redlistCategory")
-//         .then(data => {
-//             return dataFilterLogic(data, filter)
-//         })
-//         //     // makes an array of all class names
-//         //     const classes = data.map(item => {
-//         //         return item.className            
-//         //     });
-//         //     // filters through to create an array of distinct class names
-//         //     const uniqueClasses = classes.filter((name, i) => {
-//         //         return classes.indexOf(name) === i
-//         //     });
-//         //     // The start of formatting the data
-//         //     const classObj = uniqueClasses.map(item => {
-//         //         return {
-//         //             class: item,
-//         //             species: []
-//         //         }
-//         //     });
-//         //    data.map(item=> {
-//         //         const specie = item
-//         //         classObj.map(tax => {
-//         //             if(specie.className === tax.class){
-//         //                 tax.species.push(specie)
-//         //             }
-//         //         })
-//         //     })
-//         //     return classObj
-//         // })
-// }
-
-// function findByhabitat(filter){
-//     return db("assessments")
-//         .join("habitats","assessments.scientificName", "habitats.scientificName")
-//         .join("countries", "assessments.scientificName", "countries.scientificName")
-//         .whereIn("assessments.redlistCategory",["Critically Endangered", "Endangered", "Vulnerable"])
-//         .andWhere(function(){
-//             this.whereIn("countries.name", crbArry)
-//                 .andWhere(function(){
-//                     this.whereIn("habitats.code", habitatCodes)
-//                 })
-//         })
-//         .distinct("assessments.scientificName")
-//         .select("redlistCategory", "countries.name as country", "habitats.name as habitat", "habitats.code")
-//         .then(data => {
-//             // The start of formatting the data
-//             const habitatObj = habitatCodes.map(item => {
-//                 return {
-//                     habitatCode: item,
-//                     species: []
-//                 }
-//             });
-//            data.map(item=> {
-//                 const specie = item
-//                 habitatObj.map(tax => {
-//                     if(specie.code === `${tax.habitatCode}`){
-//                         tax.species.push(specie)
-//                     }
-//                 })
-//             })
-//             return habitatObj
-//         })
-// }
-
-
-
-
+ 
 function findThreatenedBy(filter){
     return db("assessments as a")
     .join("habitats as h","a.scientificName", "h.scientificName")
@@ -154,7 +23,6 @@ function findThreatenedBy(filter){
             this.whereIn("h.code", habitatCodes)
         })
     })
-    // .distinct("assessments.scientificName")
     .select("a.redlistCategory as redlistRank", 
             "c.name as country", 
             "h.name as habitat", 
@@ -237,46 +105,5 @@ function dataFilterLogic(data, filter){
         return data
     }
 };
-
-
-
-//============================ helper functions --------------
-
-function promiseCheck(res, array, crbArry){
-    if(array.length === crbArry.length){
-        res.status(200).json(array)
-    }
-}
-
-
-// function returnThreatenedSpeciesByACountry(country){
-//     db() // returns taxonomy class counts for a country
-//         .then((findByCountry) => {
-//             return {
-//                 country: country, //string
-//                 threat: threat //array
-//             }
-//         })
-// }
-
-// function returnClassCountsForCongoRiverBasis(){
-//     let crb = [
-//         "Republic of Congo",
-//         "Cameroon",
-//         "Gabon"
-//     ]
-
-//     let taxonomyCounts = []
-
-//     crb.forEach(() => {
-//         returnClassCountsByACountry()
-//             .then((countryClassCount)=> {
-//                 taxonomyCounts.push(countryClassCount)
-//             })
-//     })
-
-//     return taxonomyCounts
-
-
 
 
--- routes/threats/threats.js
+++ routes/threats/threats.js
@@ -2,78 +2,7 @@ const express = require('express');
 const router = express.Router();
 const threats = require('./threats-model')
 
-router.get('/', (req, res) => {
-    threats.find().then(threat => {
-        res.status(200).json(threat);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({errorMessage:'Failed To Get Data'})
-    })
-});
-
-
-router.get('/endangered', (req, res) => {
-    threats.findbyEndangered().then(endang => {
-        res.status(200).json(endang);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({errorMessage:'Failed To Get Data'})
-    })
-});
-
-
-router.get('/country', (req, res) => {
-    threats.findByCountry()
-        .then(data => {
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error:err})
-        })
-});
-
-router.get('/taxonomic', (req, res) => {
-    threats.findByTaxonomicRank().then(taxon => {
-        res.status(200).json(taxon);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({errorMessage:'Failed To Get Data'})
-    })
-});
-
-
-router.get('/habitats', (req, res) => {
-    threats.findByhabitat()
-        .then(data => {
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err)
-        })
-});
-
-
-router.get('/endangered/:filter' ,(req,res) => {
-    const filter = req.params.filter
-    console.log(filter)
-    if(filter === 'country'){
-        threats.findByCountry().then(count => {
-            res.status(200).json(count);
-        })
-    } else if (filter === 'habitat') {
-        threats.findByhabitat().then(habi => {
-            res.status(200).json(habi);
-        })
-    }   else if (filter === 'taxonomy') {
-        threats.findByTaxonomicRank('taxonomy').then(tax => {
-            res.status(200).json(tax);
-        })
-    }
-});
-
-router.get('/test/:filter', (req, res) => {
+router.get('/:filter', (req, res) => {
     threats.findThreatenedBy(`${req.params.filter}`)
         .then(data => {
             res.status(200).json(data)
@@ -83,4 +12,5 @@ router.get('/test/:filter', (req, res) => {
             res.status(500).json({'error':err})
         })
 });
+
 module.exports = router;
