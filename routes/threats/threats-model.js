2d02aa27648d1b5a421c33367bfef0e1260eb67f
Kris Monroe <krisdmonroe@gmail.com>
2020-04-10 12:28:49 -0700

    Optimized and added comments

--- routes/threats/threats-model.js
+++ routes/threats/threats-model.js
@@ -6,8 +6,9 @@ module.exports = {
     findThreatenedBy
 };
 
+// habitat codes that are specific to the areas of concern for project canopy
 const habitatCodes = [1.5, 1.6, 1.7, 1.8, 1.9, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 14.6];
-
+// countries that are specific to the areas of concern for project canopy
 const crbArry = ['Cameroon', 'Congo, The Democratic Republic of the', 'Gabon', 'Congo', 'Central African Republic', 'Equatorial Guinea'];
 
  
@@ -18,7 +19,9 @@ function findThreatenedBy(filter){
     .join("taxonomy as t", "a.scientificName", "t.scientificName")
     .whereIn("a.redlistCategory",["Critically Endangered", "Endangered", "Vulnerable"])
     .andWhere(function(){
+        // this subquery futher specifies the results by limiting to the countries Project Canopy is concerned with
         this.whereIn("c.name", crbArry)
+        // this subquery futher specifies the results by limiting to the habitats Project Canopy is concerned with (by the habitat code)
         .andWhere(function(){
             this.whereIn("h.code", habitatCodes)
         })
@@ -31,14 +34,17 @@ function findThreatenedBy(filter){
             "t.className"
         )
     .then(data => {
+        // this function formats the returned data based on the filter parameter
        return dataFilterLogic(data, filter)
     })
     
 }
-
+// Used to filter the data based on params
 function dataFilterLogic(data, filter){
     if(filter === 'countries'){
+        // The start of formatting the data
         const countryObj = crbArry.map(item => {
+            //The setup of the returned object with item being the country name
             return {
                 country: item,
                 species: []
@@ -47,6 +53,7 @@ function dataFilterLogic(data, filter){
         data.map(item => {
             const species = item;
             countryObj.map(obj => {
+                //sorting the species into the right country and pushing the species into the array
                 if(species.country === obj.country){
                     obj.species.push(species)
                 }
@@ -58,15 +65,19 @@ function dataFilterLogic(data, filter){
     if(filter === 'habitats'){
         // The start of formatting the data
         const habitatObj = habitatCodes.map(item => {
+            // the setup of the array of objects to be returned, each object will have a unique habitat code and an empty array
             return {
                 habitatCode: item,
                 species: []
             }
         });
-       data.map(item=> {
+        // We are mapping through the data to be able to filter and format it.
+        data.map(item=> {
             const specie = item
+            // mapping through the habitatObj and comparing the habitat code of each species and habitatObj objects to sort/filter the data in the correct format.
             habitatObj.map(tax => {
                 if(specie.code === `${tax.habitatCode}`){
+                    // if the codes match, the species is pushed into the empty array of the habitatObj object
                     tax.species.push(specie)
                 }
             })
@@ -75,7 +86,6 @@ function dataFilterLogic(data, filter){
     };
 
     if(filter === 'taxonomy'){
-        console.log('in the filter!', data)
         const classes = data.map(item => {
             return item.className            
         });
@@ -83,7 +93,7 @@ function dataFilterLogic(data, filter){
         const uniqueClasses = classes.filter((name, i) => {
             return classes.indexOf(name) === i
         });
-        // The start of formatting the data
+        // The start of formatting the data 
         const classObj = uniqueClasses.map(item => {
             return {
                 class: item,
@@ -94,6 +104,7 @@ function dataFilterLogic(data, filter){
             const specie = item
             classObj.map(tax => {
                 if(specie.className === tax.class){
+                    //if the classnames match it will be pushed into empty array classObj
                     tax.species.push(specie)
                 }
             })
