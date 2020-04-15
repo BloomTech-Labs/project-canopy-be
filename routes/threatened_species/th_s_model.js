7dbbd70d79a04097527b36b43d222a53a189850e
Chris Engel <Chris.Engel@yahoo.com>
2020-04-15 13:33:13 -0400

    removed more unneeded/experimental code. Changed order of the return of some items for front-end's convinence

--- routes/threatened_species/th_s_model.js
+++ routes/threatened_species/th_s_model.js
@@ -10,7 +10,7 @@ module.exports = {
 
 const habitatCodes = [1.5, 1.6, 1.7, 1.8, 1.9, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 14.6];
 const crbArry = ['Cameroon', 'Congo, The Democratic Republic of the', 'Gabon', 'Congo', 'Central African Republic', 'Equatorial Guinea'];
-const taxClass = ["MAMMALIA", "AVES", "REPTILIA", "AMPHIBIA"];
+const taxClass = ["AMPHIBIA", "AVES", "MAMMALIA", "REPTILIA"];
 const redlistRanks = ["Critically Endangered", "Endangered", "Vulnerable"];
 
 function allClassCountByCountry(){
